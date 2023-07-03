639ecd4f528b6a2a812ff1dd0b166d99d20d4200
arju10 <arjuarju71@gmail.com>
2023-07-03 16:07:12 +0600

    get all users, get single user, delete user, update user

--- src/User/User.controller.ts
+++ src/User/User.controller.ts
@@ -1,4 +1,4 @@
-import {Request, Response } from 'express';
+import {NextFunction, Request, Response } from 'express';
 import UserModel from './User.model';
 
 // Create a new user => Route: /api/v1/auth/signup (POST)
@@ -16,7 +16,114 @@ export const createUser = async (req: Request, res: Response) => {
     res.status(500).json({ 
         success: false,
         statusCode: 500,
-        message: 'Internal server error' });
+        message: 'Failed to create user' });
   }
 };
 
+ // Get all Users => Route: /api/v1/users (GET)
+ export const getAllUsers = async (req: Request, res: Response,Next:NextFunction) => {
+  try {
+    const users = await UserModel.find();
+    res.status(200).json({
+      success: true,
+      statusCode: 200,
+      message: 'Users retrieved successfully',
+      data: users,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      statusCode: 500,
+      message: 'Failed to retrieve users',
+      error: error
+    });
+  }
+};
+
+// Get a Single User => Route: /api/v1/users/:id (GET)
+export const getSingleUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const user = await UserModel.findById(id);
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        statusCode: 404,
+        message: 'User not found',
+        data: null,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      statusCode: 200,
+      message: 'User retrieved successfully',
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      statusCode: 500,
+      message: 'Failed to retrieve user',
+      error: error
+    });
+  }
+};
+
+// Update a Single User => Route: /api/v1/users/:id (PATCH)
+export const updateSingleUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const updatedUser = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+    if (!updatedUser) {
+      return res.status(404).json({
+        success: false,
+        statusCode: 404,
+        message: 'User not found',
+        data: null,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      statusCode: 200,
+      message: 'User updated successfully',
+      data: updatedUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      statusCode: 500,
+      message: 'Failed to update user',
+      error: error
+    });
+  }
+};
+
+
+// Delete a User => Route: /api/v1/users/:id ( DELETE)
+export const deleteSingleUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const deletedUser = await UserModel.findByIdAndDelete(id);
+    if (!deletedUser) {
+      return res.status(404).json({
+        success: false,
+        statusCode: 404,
+        message: 'User not found',
+        data: null,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      statusCode: 200,
+      message: 'User deleted successfully',
+      data: deletedUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      statusCode: 500,
+      message: 'Failed to delete user',
+      error: error
+    });
+  }
+};
--- src/User/User.route.ts
+++ src/User/User.route.ts
@@ -1,7 +1,19 @@
 import express from 'express';
-import { createUser } from './User.controller';
+import { createUser, deleteSingleUser, getAllUsers, getSingleUser, updateSingleUser } from './User.controller';
 const router = express.Router();
 
 router.post('/auth/signup', createUser);
+
+// Get all Users
+router.get('/users',getAllUsers);
+
+// Get a Single User
+router.get('/users/:id',getSingleUser);
+
+// Update a Single User
+router.patch('/users/:id',updateSingleUser);
+
+// Delete a User
+router.delete('/users/:id',deleteSingleUser);
 
 export default router;
